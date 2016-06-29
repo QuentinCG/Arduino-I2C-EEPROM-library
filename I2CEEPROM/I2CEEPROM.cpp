43f1e6d26e8537e888c99641d7f0882acacfbf21
Quentin Comte-Gaz <quentin@comte-gaz.com>
2016-06-29 18:38:37 +0200

    lib: Add main I2C EEPROM library
    Add the most important part of this project (the lib).
    A keywords.txt file has been added to improve lib display
    in arduino interface.

---
+++ I2CEEPROM/I2CEEPROM.cpp
@@ -0,0 +1,50 @@
+/*
+ * \brief I2C EEPROMs data writer/reader (implementation)
+ *
+ * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
+ * \date 29 June 2016
+ * \license MIT License (contact me if too restrictive)
+ * \copyright Copyright (c) 2016 Quentin Comte-Gaz
+ * \version 1.0
+ */
+
+#include "Arduino.h"
+#include "I2CEEPROM.h"
+#include <Wire.h>
+
+I2CEEPROM::I2CEEPROM(int i2c_device_address)
+{
+  _i2c_device_address = i2c_device_address;
+  Wire.begin();
+}
+
+void I2CEEPROM::write(unsigned int address, byte data)
+{
+  Wire.beginTransmission(_i2c_device_address);
+  Wire.write((int)(address >> 8));   // MSB
+  Wire.write((int)(address & 0xFF)); // LSB
+  Wire.write(data);                  // Write byte
+  Wire.endTransmission();
+
+  // Writing in I2C EEPROM takes ~5ms (even if I2C writing already done)
+  delay(5);
+}
+
+byte I2CEEPROM::read(unsigned int address)
+{
+  byte read_data = 0xFF;
+
+  Wire.beginTransmission(_i2c_device_address);
+  Wire.write((int)(address >> 8));   // MSB
+  Wire.write((int)(address & 0xFF)); // LSB
+  Wire.endTransmission();
+
+  // Request 1 byte from device
+  Wire.requestFrom(_i2c_device_address, 1);
+
+  if (Wire.available()) {
+    read_data = Wire.read();
+  }
+
+  return read_data;
+}
---
+++ I2CEEPROM/I2CEEPROM.h
@@ -0,0 +1,44 @@
+/*
+ * \brief I2C EEPROMs data writer/reader library
+ *
+ * \author Quentin Comte-Gaz <quentin@comte-gaz.com>
+ * \date 29 June 2016
+ * \license MIT License (contact me if too restrictive)
+ * \copyright Copyright (c) 2016 Quentin Comte-Gaz
+ * \version 1.0
+ */
+
+#ifndef I2CEEPROM_h
+#define I2CEEPROM_h
+
+#include "Arduino.h"
+
+class I2CEEPROM
+{
+  public:
+    /*!
+     * \brief I2CEEPROM Initialize I2C EEPROM instance
+     * \param device_address (int) I2C address of the EEPROM device
+     */
+    I2CEEPROM(int i2c_device_address);
+
+    /*!
+     * \brief write Write one byte \p data in EEPROM device at EEPROM internal address \p address
+     * \param address (unsigned int) EEPROM internal address (most of the time, first address is 0x00)
+     * \param data (byte) Byte to write at EEPROM internal address \p address
+     */
+    void write(unsigned int address, byte data);
+
+    /*!
+     * \brief read Read one byte in EEPROM device at EEPROM internal address \p address
+     * \param address (unsigned int) EEPROM internal address (most of the time, first address is 0x00)
+     * \return (byte) Read Byte at EEPROM internal address \p address (returns 0xFF if an error occurred)
+     */
+    byte read(unsigned int address);
+
+  private:
+    int _i2c_device_address;
+};
+
+#endif //I2CEEPROM_h
+
